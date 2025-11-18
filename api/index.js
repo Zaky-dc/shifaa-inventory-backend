46b36819b29fb0f5882b909b3db4563124e31f08
ZakirMagide <62655140+Zaky-dc@users.noreply.github.com>
2025-11-18 15:36:59 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 //import dotenv from "dotenv";
-import express from "express";
+//import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
 //dotenv.config();
 
-const app = express();
+//const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,7 +18,5 @@ app.use("/api/contagem", contagemRouter);
 app.use("/api/datas", datasRouter);
 app.use("/api/armazens", armazensRouter);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor local rodando na porta ${PORT}`);
-});
+//const PORT = process.env.PORT || 4000;
+
