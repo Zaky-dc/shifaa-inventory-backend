8f08fc3b82e9e7d305e39d58df495ecd824d59db
ZakirMagide <62655140+Zaky-dc@users.noreply.github.com>
2025-11-18 15:28:59 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Importa rotas simulando Vercel
-app.use('/api/contagem', require('./contagem'));
-app.use('/api/datas', require('./datas'));
-app.use('/api/armazens', require('./armazens'));
+import contagemRouter from "./api/contagem.js";
+import datasRouter from "./api/datas.js";
+import armazensRouter from "./api/armazens.js";
+
+app.use("/api/contagem", contagemRouter);
+app.use("/api/datas", datasRouter);
+app.use("/api/armazens", armazensRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(` Servidor local rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor local rodando na porta ${PORT}`);
 });
